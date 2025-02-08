98895692f4100f15e0aa8978743f7a2305bd9847
Joseph <joseph@joseph-rock.com>
2025-02-08 15:02:29 -0800

    Add lsp

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -29,6 +29,24 @@ require("lazy").setup({
     end,
   },
   {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "folke/lazydev.nvim",
+      ft = "lua", -- only load on lua files
+      opts = {
+        library = {
+          -- See the configuration section for more details
+          -- Load luvit types when the `vim.uv` word is found
+          { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+        },
+      },
+    },
+    config = function()
+      require("lspconfig").lua_ls.setup {}
+      require("lspconfig").rust_analyzer.setup {}
+    end,
+  },
+  {
     "numToStr/Comment.nvim",
     config = function()
       require("Comment").setup()
