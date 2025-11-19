aa5fdd51f31573041db80cbb5869aa2592148e4f
Joseph <josephrock225@gmail.com>
2025-11-18 17:51:07 -0800

    Small changes

--- neovim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ neovim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,11 +1,14 @@
 return {
   "https://github.com/nvim-treesitter/nvim-treesitter",
-  modules = {},
-  ensure_installed = {
-    "lua", "python", "rust", "json", "markdown",
-    "markdown_inline", "html", "yaml",
-  },
-  sync_install = false,
-  ignore_install = {},
-  auto_install = true,
+  opts = {
+    build = ":TSUpdate",
+    modules = {},
+    ensure_installed = {
+      "lua", "python", "rust", "json", "markdown",
+      "markdown_inline", "html", "yaml", "html", "latex", "yaml",
+    },
+    sync_install = false,
+    ignore_install = {},
+    auto_install = true,
+  }
 }
