eac1cbf15f8c9b4d1d2f10ce34efcabf5b18e574
Joseph <josephrock225@gmail.com>
2025-11-20 11:16:15 -0800

    Add hypridle

--- neovim/.config/nvim/lua/plugins/mini-nvim.lua
+++ neovim/.config/nvim/lua/plugins/mini-nvim.lua
@@ -1,10 +1,11 @@
-return { 
-  'nvim-mini/mini.nvim', 
+return {
+  'nvim-mini/mini.nvim',
   version = false,
   config = function()
-     require("mini.icons").setup({})
-     require("mini.surround").setup({})
-     -- require("mini.files").setup({})
-     require("mini.tabline").setup({})
-   end
+    require("mini.icons").setup()
+    -- require("mini.ai").setup()
+    require("mini.surround").setup()
+    -- require("mini.files").setup()
+    require("mini.tabline").setup()
+  end
 }
