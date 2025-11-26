3824024932f513e4a91a612e62e5594d5fa02297
Joseph <josephrock225@gmail.com>
2025-11-26 08:48:01 -0800

    Fix cssls

--- neovim/.config/nvim/lua/plugins/mason-lspconfig.lua
+++ neovim/.config/nvim/lua/plugins/mason-lspconfig.lua
@@ -2,7 +2,7 @@ return {
   "mason-org/mason-lspconfig.nvim",
   opts = {
     ensure_installed = {
-      "lua_ls", "pyright", "rust_analyzer", "jsonls", "bashls", "ruff", "marksman", "css-lsp"
+      "lua_ls", "pyright", "rust_analyzer", "jsonls", "bashls", "ruff", "marksman", "cssls"
     },
   },
   dependencies = {
